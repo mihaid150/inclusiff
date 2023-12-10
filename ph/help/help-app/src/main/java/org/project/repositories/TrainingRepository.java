df424f501fb4368c47fc5ed243c619908aa7b5a4
Cristian Balea <127232514+cristianbalea@users.noreply.github.com>
2023-12-10 13:28:50 +0200

    [BE] Add new API for user trainings (#1)

--- ph/help/help-app/src/main/java/org/project/repositories/TrainingRepository.java
+++ ph/help/help-app/src/main/java/org/project/repositories/TrainingRepository.java
@@ -4,6 +4,10 @@ import org.project.entities.TrainingEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+import java.util.UUID;
+
 @Repository
 public interface TrainingRepository extends JpaRepository<TrainingEntity, Integer> {
+    Optional<TrainingEntity> findByTrainingExternalId(UUID trainingExternalId);
 }
--- ph/help/help-app/src/main/java/org/project/services/HelpService.java
+++ ph/help/help-app/src/main/java/org/project/services/HelpService.java
@@ -5,13 +5,17 @@ import org.mapstruct.factory.Mappers;
 import org.project.dtos.ResponseMessage;
 import org.project.dtos.TrainingDto;
 import org.project.dtos.TrainingRegisterRequest;
+import org.project.entities.TrainingEntity;
 import org.project.entities.TrainingRegistrationEntity;
 import org.project.repositories.TrainingRegistrationRepository;
 import org.project.repositories.TrainingRepository;
 import org.project.services.mappers.TrainingMapper;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Service
@@ -21,7 +25,11 @@ public class HelpService {
     private final TrainingRegistrationRepository trainingRegistrationRepository;
     private final TrainingMapper trainingMapper = Mappers.getMapper(TrainingMapper.class);
 
+    @Transactional
     public ResponseMessage registerToTraining(TrainingRegisterRequest request) {
+        if (getTrainingParticipants(request.getTrainingExternalId()).contains(request.getUserExternalId())) {
+            throw new RuntimeException("You cannot register for the same training twice!");
+        }
         trainingRegistrationRepository.save(TrainingRegistrationEntity.builder()
                 .trainingExternalId(request.getTrainingExternalId())
                 .userExternalId(request.getUserExternalId())
@@ -39,5 +47,27 @@ public class HelpService {
     public List<UUID> getTrainingParticipants(UUID trainingExternalId) {
         return trainingRegistrationRepository.findAllByTrainingExternalId(trainingExternalId).stream()
                 .map(TrainingRegistrationEntity::getUserExternalId).toList();
+    }
+
+    public List<TrainingDto> getUserTrainings(UUID userExternalId) {
+        List<UUID> trainings =  trainingRegistrationRepository.findAllByUserExternalId(userExternalId).stream()
+                .map(TrainingRegistrationEntity::getTrainingExternalId)
+                .toList();
+
+        List<TrainingDto> trainingDtoList = new ArrayList<>(trainings.stream().map((id) -> {
+                    Optional<TrainingEntity> tr = trainingRepository.findByTrainingExternalId(id);
+                    return tr.map(trainingMapper::toDto).orElse(null);
+                }
+        ).toList());
+
+        List<TrainingDto> nulls = new ArrayList<>();
+        for (TrainingDto t : trainingDtoList) {
+            if (t == null) {
+                nulls.add(t);
+            }
+        }
+
+        trainingDtoList.removeAll(nulls);
+        return trainingDtoList;
     }
 }
